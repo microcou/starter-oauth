51b80e6ffc17ee2269f0c412936e6379300bc00b
LC <coulet4@gmail.com>
2023-04-05 11:46:08 +0200

    init

---
+++ server.js
@@ -0,0 +1,74 @@
+const crypto = require("node:crypto");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const CONFIG = {
+	port: 3000,
+	client_id: "...",
+	client_secret: "...",
+	redirect_uri: "http://localhost:3000/oauthcallback",
+};
+
+const app = express();
+
+app.use(cookieParser());
+
+app.get("/connect", function (req, res) {
+	const state = crypto.randomBytes(32).toString("hex");
+
+	const url =
+		"https://www.linkedin.com/oauth/v2/authorization" +
+		`?client_id=${CONFIG.client_id}` +
+		`&redirect_uri=${CONFIG.redirect_uri}` +
+		`&state=${state}` +
+		"&response_type=code" +
+		"&scope=r_liteprofile,w_member_social,rw_organization_admin";
+
+	res.cookie("oauth_state", state, { httpOnly: true });
+	res.redirect(url);
+});
+
+app.get("/oauthcallback", async function (req, res) {
+	const { state, code } = req.query;
+	const cookieState = req.cookies["oauth_state"];
+
+	if (state !== cookieState) {
+		return res.status(403).send("state mismatch"); // maybe CSRF
+	}
+
+	const formData =
+		`client_id=${CONFIG.client_id}` +
+		`&client_secret=${CONFIG.client_secret}` +
+		`&redirect_uri=${CONFIG.redirect_uri}` +
+		`&code=${code}` +
+		"&grant_type=authorization_code";
+
+	const tokenRes = await fetch("https://www.linkedin.com/oauth/v2/accessToken", {
+		method: "POST",
+		body: formData,
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+	});
+
+	const tokenData = await tokenRes.json();
+	console.log("token data", tokenData);
+
+	// {
+	//   access_token: 'AQWuz...',
+	//   expires_in: 5183999,
+	//   refresh_token: 'AQWT2...',
+	//   refresh_token_expires_in: 31535999,
+	//   scope: 'r_liteprofile,rw_organization_admin,w_member_social'
+	// }
+
+	res.send("Connected successfully!");
+});
+
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(CONFIG.port, function () {
+	console.log(`Server listening at http://localhost:${CONFIG.port}`);
+});
